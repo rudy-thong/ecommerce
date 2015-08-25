0b66b916c486b364299c448cb195474cf0afd271
andythong <andy.thong8@gmail.com>
2015-08-25 11:06:47 +0700

    latihan guest book

---
+++ practice/PHP-simple-guestbook-form/Latihan/latihan.php
@@ -0,0 +1,31 @@
+</pre>
+<form action="addguestbook.php" method="post" name="guest">
+<table width="1000" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+<tr>
+<td width="117">Name</td>
+<td width="14">:</td>
+<td width="357"><input name="name" type="text" id="name" size="40" STYLE="color: black; font-family: Times N font-size: 12px; background-color: yellow;" ; /></td>
+</tr>
+<tr>
+<td width="117">Email</td>
+<td width="14">:</td>
+<td width="357"><input name="email" type="email" id="email" size="40" STYLE="color: black; font-family: Times N font-size: 12px; background-color: yellow; placeholder="me@example.com" /></td>
+</tr>
+<tr>
+<td width="117">Phone Number</td>
+<td width="14">:</td>
+<td width="357"><input name="phone" type="text" id="phone" size="40" STYLE="color: black; font-family: Times N font-size: 12px; background-color: yellow;" /></td>
+</tr>
+<tr>
+<td valign="top">Comment</td>
+<td valign="top">:</td>
+<td><textarea name="comment" size="40" rows="3" id="comment" STYLE="color: black; font-family: Times N font-size: 12px; background-color: yellow;"></textarea></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type="submit" name="Submit" value="Submit" /> <input type="reset" name="Submit2" value="Reset" /></td>
+</tr>
+</table>
+</form>
+<pre>
